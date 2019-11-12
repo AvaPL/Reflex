40a5e58057a1c1a869b170d83d384ab6f1fa577a
AvaPL <241574@student.pwr.edu.pl>
2019-11-12 14:49:17 +0100

    Buzzer and its tests added.

---
+++ Code/lib/Buzzer/src/Buzzer.h
@@ -0,0 +1,17 @@
+#include <Arduino.h>
+
+class Buzzer
+{
+
+private:
+    byte pin;
+
+public:
+    const static unsigned int TONE_LOW = 1000;
+    const static unsigned int TONE_MEDIUM = 2000;
+    const static unsigned int TONE_HIGH = 3000;
+
+public:
+    Buzzer(byte pin) : pin(pin) {}
+    void playTone(unsigned int frequency, unsigned long duration);
+};
